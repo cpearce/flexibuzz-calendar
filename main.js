e6d317a2f4ca303d9e6d11ec834aa81e6c000d6e
Chris Pearce <cpearce@mozilla.com>
2018-04-01 12:16:53 +1200

    Collect all data necessary to post recurring events.

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function init() {
     e("password").value = password;
   }
   if (username && password) {
-    login().then(listCalendar);
+    login().then(listCalendar).then(setupNewEventForm); // TODO: This needs to happen on the non-restore case too!
   }
 }
 
@@ -65,19 +65,29 @@ async function login() {
   })
 }
 
-async function listBoxes() {
+async function setupNewEventForm() {
+  // Setup list of boxes.
   let boxes = await tiqbiz.boxes();
-  log("Boxes:");
+  let boxList = e('box-list');
+  clearChildren(boxList);
   for (var box of boxes) {
-    var details = "";
-    for (var property in box) {
-      if (details.length > 0) {
-        details += ",";
-      }
-      details += property + " = " + box[property];
-    }
-    log(details);
+    var id = "box-list-" + box.id;
+    var div = document.createElement("div");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = id;
+    checkbox.value = id;
+    checkbox.boxId = box.id;
+    div.appendChild(checkbox);
+    var label = document.createElement("label");
+    label.appendChild(document.createTextNode(box.name));
+    label.for = id;
+    div.appendChild(label);
+    boxList.appendChild(div);
   }
+  // Ensure the start/end times are enabled/disabled as expected.
+  allDayChanged();
+  updateRecurrence();
 }
 
 function clearChildren(node) {
@@ -109,5 +119,141 @@ async function listCalendar() {
     [entry.title, extractDate(entry), entry.boxes.join(", ")]
       .map(text => addTd(tr, text));
     calendarDiv.appendChild(tr);
+  }
+}
+
+function makeDate(date, time, allDay) {
+  return date + " " + (allDay ? "00:00:00" : time);
+}
+
+function makeShortDateTime(d) {
+  return makeShortDate(d) + " " + makeShortTime(d);
+}
+
+function makeShortTime(d) {
+  return fw(d.getHours()) + ":" + fw(d.getMinutes()) + ":" + fw(d.getSeconds());
+}
+
+function fw(n) {
+  if (n < 10) {
+    return "0" + n;
+  }
+  return "" + n;
+}
+
+function makeShortDate(d) {
+  // 2018-03-30 15:24:42
+  return d.getFullYear() + "-" + fw(d.getMonth() + 1) + "-" + fw(d.getDate());
+}
+
+async function createCalendarEvent() {
+  // 2018-05-11 00:00:00
+  // all day events don't have a time, just date.
+
+  // Figure out which boxes are checked.
+  let boxes = [];
+  var checkboxes = document.querySelectorAll('#box-list > div > input[type="checkbox"]');
+  for (var checkbox of checkboxes) {
+    if (checkbox.checked) {
+      boxes.push(checkbox.boxId);
+    }
+  }
+
+  // Figure our recurrent instances.
+  let repetitions = [];
+  for (var span of document.querySelectorAll(".recurrent-event-repetition")) {
+    repetitions.push(span.textContent);
+  }
+
+  boxes = boxes.join(","); // Figure out multi-format for boxes.
+  let allDay = e("allDay").value == "on" ? true : false;
+
+  for (var date of repetitions) {
+    // Figure out what notifications are selected.
+    let notifications = [];
+    if (e("notify-day-before").checked) {
+      let d = makeShortDate(addDays(new Date(date), -1));
+      notifications.push(makeDate(d, "10:00:00", false));
+    }
+    if (e("notify-24-hours-before").checked) {
+      let d = makeShortDate(addDays(new Date(date), -1));
+      notifications.push(makeDate(d, e("startTime").value, false));
+    }
+    if (e("notify-1-hour-before").checked) {
+      let timeStr = date + " " + e("startTime").value;
+      let d = makeShortDateTime(addHours(new Date(timeStr), -1));
+      notifications.push(d);
+    }
+
+    let event = {
+      boxes: boxes,
+      post_type: "calendar",
+      title: e("title").value,
+      body_markdown: e("description").value,
+      start_date: makeDate(date, e("startTime").value, allDay),
+      end_date: makeDate(date, e("endTime").value, allDay),
+      all_day: allDay,
+      published_at: makeShortDateTime(new Date()),
+    };
+    if (e("location").value.length > 0) {
+      event["location"] = e("location").value;
+    }
+    if (e("address").value.length > 0) {
+      event["address"] = e("address").value;
+    }
+    // event["notifications[]"] = "2018-05-10 10:00:00"; // 24 hours beforehand
+    log(JSON.stringify(event));
+    log("notifications at: " + notifications.join(","));
+
+  }
+
+}
+
+function allDayChanged(event) {
+  let allDayCheckbox = e("allDay");
+  if (allDayCheckbox.checked) {
+    e("startTime").disabled = true;
+    e("endTime").disabled = true;
+  } else {
+    e("startTime").disabled = false;
+    e("endTime").disabled = false;
+  }
+}
+
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+function addHours(date, hours) {
+  var result = new Date(date);
+  result.setHours(result.getHours() + hours);
+  return result;
+}
+
+function updateRecurrence() {
+  let from = new Date(e("startDate").value);
+  e("recurrence-starting-date").innerHTML = e("startDate").value;
+  let select = e("recurrence-select");
+  let interval = 7 * parseInt(select.options[select.selectedIndex].value);
+  let to = new Date(e("recurrence-ending-date").value);
+  let list = e("recurrence-event-repetitions");
+  clearChildren(list);
+  let d = new Date(from);
+  while (d <= to) {
+    var div = document.createElement("div");
+    var span = document.createElement("span");
+    span.appendChild(document.createTextNode(makeShortDate(d)));
+    span.classList.add("recurrent-event-repetition");
+    div.appendChild(span);
+    var removeButton = document.createElement("button");
+    removeButton.appendChild(document.createTextNode("Remove"));
+    removeButton.addEventListener("click", (event) => {
+      div.parentNode.removeChild(div);
+    }, false);
+    div.appendChild(removeButton);
+    list.appendChild(div);
+    d = addDays(d, interval);
   }
 }
